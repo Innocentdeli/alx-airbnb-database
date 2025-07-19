d6f5c784e287c4b9581de4363e8f71f62c705968
Innocentdeli <innocentdeli1@gmail.com>
2025-07-19 14:07:58 -0700

    Refactor complex queries to improve performance.

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -15,6 +15,8 @@ FROM bookings b
 JOIN users u ON b.user_id = u.user_id
 JOIN properties p ON b.property_id = p.property_id
 LEFT JOIN payments pay ON b.booking_id = pay.booking_id;
+WHERE b.booking_date >= CURRENT_DATE - INTERVAL '30 days'
+  AND pay.amount > 100;
 
 EXPLAIN ANALYZE
 SELECT
